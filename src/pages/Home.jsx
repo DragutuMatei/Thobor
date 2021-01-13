a3a7739527ad14a852fcd318a58f300eb28191ac
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-01-13 13:35:44 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ function Home() {
   return (
     <>
       <Landing />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptate, consequatur quisquam ipsam voluptatem rerum a odit architecto est dignissimos velit nesciunt dolorem eum sit iure soluta illum quam obcaecati! Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos, ea non, reprehenderit assumenda nulla praesentium tempora dolore debitis ab vel earum facilis molestias illum tempore perferendis sit aliquam provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis illo velit, aliquam aperiam, ipsum quod nisi, neque quisquam quibusdam iste iusto optio. Repellat natus optio architecto ea a sapiente pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero quaerat ipsa soluta impedit deleniti vitae, suscipit enim architecto harum velit vel placeat laudantium tenetur itaque. Suscipit blanditiis saepe quam?
       <Stem />
       <Contact />
       <Membrii ok={false} />
