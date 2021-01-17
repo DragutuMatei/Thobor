ff249a9e827e20e1d84065a74e6fc8f91e9a8397
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-01-17 22:44:13 +0200

    asd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,6 +42,7 @@ function Footer() {
             <Link to={"/blog/" + p.titlu}>{p.titlu}</Link>
           ))}
         </div>
+        <div className="right"></div>
       </div>
       <h4 className="bottom">
         THOBOR | Echipa de robotică a Colegiului Național “Calistrat Hogaș”
