7267e0dd108898acbad16d01b7dced9db8938166
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-01-19 20:51:00 +0200

    asd

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -7,9 +7,9 @@ function Ads() {
       <AdSense.Google
         className="right"
         style={{ width: 300, height: 300 }}
-        client="5123103463"
+        client="ca-pub-7900802648960033"
         format=""
-        slot="8761774704"
+        slot="5123103463"
       />
     </>
   );
