925648746d856f0d5c96318d6a60ac5be89ab3a7
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-01-19 13:20:34 +0200

    asd

--- src/components/Apps/Aplicatie.jsx
+++ src/components/Apps/Aplicatie.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Aplicatie() {
+function Aplicatie({}) {
   return (
     <>
       <div className="apl">
--- src/pages/Apps.jsx
+++ src/pages/Apps.jsx
@@ -15,6 +15,29 @@ function Apps() {
         </a>
         <img src={require("../assets/Apps/qr.jpg").default} alt="" />
       </div>
+      <div className="apl">
+        <img src={require("../../assets/Apps/scavanger.png").default} alt="" />
+        <div className="sc">
+          <h1>Aplicatia noastră - Realitate Augmentata -</h1>
+          <p>
+            Poti avea robotul Thor la tine acasa! Descarca aplicatia, printeaza
+            codul QR si ai micul tau robot in buzunar.
+          </p>
+          <p>
+            Robotica uneste lumea, iar noi, va prezentam robotul nostru din cel
+            mai recent sezon FIRST Tech Challenge.
+          </p>
+          <h4>
+            Ne poti sustine echipa pe facebook, <b> instagram</b> sau
+            <b> youtube!</b>
+          </h4>
+        </div>
+      </div>
+      <div className="qr">
+        <a href="https://thobornights.netlify.app/">
+          Thobor Nigths
+        </a>
+      </div>
       <Up/>
     </>
   );
